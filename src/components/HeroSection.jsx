2a49c3ac8827cd4fe7f0794eda0d044c9752c008
smi-23 <latack789@naver.com>
2024-01-29 06:06:36 +0900

    update: create project which is portfolio-website

---
+++ src/components/HeroSection.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const HeroSection = () => {
+    return (
+        <section><h1 className="text-white">안녕하세요 조가랍입니다.</h1>
+        <p className="text-[#ADB7BE]">여기에 소개 내용을 적습니다.</p></section>
+    );
+};
+
+export default HeroSection;
