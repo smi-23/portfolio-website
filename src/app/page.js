c91b534ed28c0ef550b66caf57e6450cbf213baf
smi-23 <latack789@naver.com>
2024-01-29 08:04:30 +0900

    update: 제목에 애니메이션 기능 추가 및 버튼 꾸밈

--- src/app/page.js
+++ src/app/page.js
@@ -2,9 +2,11 @@ import Image from "next/image"
 import HeroSection from "@/components/HeroSection";
 
 export default function Home() {
-    return (
-        <main className="flex min-h-screen flex-col bg-[#121212]">
-            <HeroSection/>
-        </main>
-    );
+  return (
+    <main className="flex min-h-screen flex-col bg-[#121212] ">
+      <container class="container mx-auto px-12 py-4">
+        <HeroSection/>
+      </container>
+    </main>
+  );
 }
--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,31 +1,62 @@
+"use client"
 import React from "react";
 import Image from "next/image";
+import {TypeAnimation} from "react-type-animation";
 
 const HeroSection = () => {
-    return (
-        <section>
-            <div className="grid grid-cols-1 lg:grid-cols-12">
-                <div className="col-span-7 place-self-center">
-                    <h1 className="text-white mb-4 text-4xl lg:text-6xl font-extrabold">안녕하세요 조가랍입니다.</h1>
-                    <p className="text-[#ADB7BE] text-lg lg:text-xl">여기에 소개 내용을 적습니다.</p>
-                </div>
-                <div className="col-span-5">
-                    <div className="rounded-full bg-[#181818] w-[250px] h-[250px] lg:w-[400px] relative">
-                        <Image
-                            src="/images/조가람.png"
-                            alt="hero image"
-                            className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
-                            width={300}
-                            height={300}
-                        />
-                    </div>
+  return (
+    <section>
+      <div className="grid grid-cols-1 sm:grid-cols-12">
+        <div className="col-span-7 place-self-center text-center sm:text-left">
+          <h1 className="text-white mb-4 text-4xl sm:text-6xl font-extrabold">
+            <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600">
+              안녕하세요!{" "}
+            </span>
+            <br/>
+            <TypeAnimation
+              sequence={[
+                "백엔드 개발자",
+                1000,
+                "조가람입니다.",
+                1000,
+              ]}
+              wrapper="span"
+              speed={50}
+              repeat={Infinity}
+            />
+          </h1>
+          <p className="text-[#ADB7BE] text-base sm:text-lg mb-6 lg:text-xl">
+            여기에 소개 내용을 적습니다.
+          </p>
+          <div>
+            <button
+              className="px-6 py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 hover:bg-slate-200 text-white">
+              Hire me
+            </button>
+            <button
+              className="px-1 py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 hover:bg-slate-800 text-white mt-3">
+              <span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2">
+              Download CV
+              </span>
+            </button>
+          </div>
+        </div>
+        <div className="col-span-5 place-self-center m4-4 lg:mt-0 ">
+          <div className="rounded-full bg-[#181818] w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative">
+            <Image
+              src="/images/조가람.png"
+              alt="hero image"
+              className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
+              width={300}
+              height={300}
+            />
+          </div>
+        </div>
+      </div>
 
-                </div>
-            </div>
 
-
-        </section>
-    );
+    </section>
+  );
 };
 
 export default HeroSection;
